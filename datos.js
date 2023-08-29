2c867f6cb5fc962a2856630a7d7c83a714f1b081
Ilesandres <125101667+Ilesandres@users.noreply.github.com>
2023-08-29 15:30:00 -0500

    Update datos.js

--- datos.js
+++ datos.js
@@ -1,30 +1,61 @@
-let nombre = document.getElementById("nombre").value;
-let semestre = document.getElementById("semestre").value;
-let respuestas = [
-    document.getElementById("R0").value,
-    document.getElementById("R1").value,
-    document.getElementById("R2").value,
-    document.getElementById("R3").value,
-    document.getElementById("R4").value,
-    document.getElementById("selected").value,
-    document.getElementById("interes").value,
-    "Andres","casa1","Casa2"
-]
-/*
-let nombre = document.getElementById("nombre").value;
-let semestre = document.getElementById("semestre").value;
-let respuestas = [
-    document.getElementById("R0").value,
-    document.getElementById("R1").value,
-    document.getElementById("R2").value,
-    document.getElementById("R3").value,
-    document.getElementById("R4").value,
-    document.getElementById("selected").value,
-    document.getElementById("interes").value
-];*/
+
+// Array para almacenar los datos de encuestas
+
+
+let encuestas = ["Andres","2",];
+
+
+// Función para guardar los datos de la encuesta
+function guardar() {
+    let nombre = document.getElementById("nombre").value;
+    let semestre = document.getElementById("semestre").value;
+    let respuestas = [
+        document.getElementById("R0").value,
+        document.getElementById("R1").value,
+        document.getElementById("R2").value,
+        document.getElementById("R3").value,
+        document.getElementById("R4").value,
+        document.getElementById("selected").value,
+        document.getElementById("interes").value
+    ]
 
 let encuesta = {
     nombre: nombre,
     semestre: semestre,
     respuestas: respuestas
 };
+   
+
+    encuestas.push(encuesta);
+
+    alert("Encuesta guardada con éxito");
+    console.log(encuestas);
+
+    // Limpiar campos después de guardar
+    limpiarCampos();
+
+    // Mostrar encuesta
+}
+
+// Función para limpiar los campos del formulario después de guardar
+function limpiarCampos() {
+    document.getElementById("nombre").value = "";
+    document.getElementById("semestre").value = "";
+    document.getElementById("R0").value = "";
+    document.getElementById("R1").value = "";
+    document.getElementById("R2").value = "";
+    document.getElementById("R3").value = "";
+    document.getElementById("R4").value = "";
+    document.getElementById("selected").value = "";
+    document.getElementById("interes").value = "";
+}
+
+// Función para mostrar la encuesta 
+
+function mostrar() {
+    for(let i=0; i<encuestas.length; i++){
+     document.getElementById("quest").innerHTML = encuestas;
+     console.log(encuestas);
+     
+    }
+}
